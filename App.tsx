3a3116d72a8a73bcdb514213d73feb97fbc53ef6
antoniiio2700 <145315007+antoniiio2700@users.noreply.github.com>
2024-02-21 11:36:02 +0800

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, TextInput, Button, StyleSheet} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
