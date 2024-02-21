48e4500ad9336bce2383233f2f4dadb2430fa18c
karlrafael-digiteer <karlrafael.digiteer@gmail.com>
2024-02-21 18:53:02 +0800

    changes made

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
-
+import { StyleSheet, View, Text, TextInput, Platform, Button } from 'react-native';
 
 const GreetingApp = () => {
   const [name, setName] = useState('');
   const [greeting, setGreeting] = useState("Hello, what's your name?");
 
-
   const handleNameChange = (text: string) => {
     setName(text);
   };
 
-
-
   const handleSubmit = () => {
     if (name) {
-
       const time = new Date().getHours();
       let timeGreeting;
       if (time >= 0 && time < 12) {
@@ -30,23 +25,56 @@ const GreetingApp = () => {
     }
   };
 
-
-
   return (
-    <View>
-      <Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>
         {greeting}
-
       </Text>
       <TextInput
         placeholder="Enter your name"
         value={name}
         onChangeText={handleNameChange}
-
+        style={styles.input}
       />
-      <Button title="Submit" onPress={handleSubmit} />
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Submit"
+          onPress={handleSubmit}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Platform.OS === 'android' ? 200 : 0,
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 25,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 10,
+    margin: 10,
+    width: '80%',
+    borderRadius: 5,
+  },
+  buttonContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  button: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+});
 
 export default GreetingApp;
