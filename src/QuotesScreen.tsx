a0a47211c2f91b576d7f0b02d23b3f2d05f7b889
antoniiio2700 <145315007+antoniiio2700@users.noreply.github.com>
2024-02-27 16:12:14 +0800

    Added Random Quote Generator API in Quotes Screen

--- src/QuotesScreen.tsx
+++ src/QuotesScreen.tsx
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@rneui/themed";
 
+type QUOTE = {
+  id: string;
+  content: string;
+  author: string;
+};
+
 export const QuotesScreen = () => {
+  const [quote, setQuote] = useState<QUOTE>();
+
+  const getQuote = async () => {
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      const json = await response.json();
+      setQuote({ id: json.id, content: json.content, author: json.author });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View>
       <View style={localStyle.container}>
-        <Text style={styles.text}>
-          Tap the button below to generate random quote
-        </Text>
+        {quote ? (
+          <>
+            <Text style={styles.text}>"{quote.content}"</Text>
+            <Text style={localStyle.author}>- {quote.author}</Text>
+          </>
+        ) : (
+          <Text style={styles.text}>
+            Tap the button below to generate random quote
+          </Text>
+        )}
+
         <Button
           title="Generate Quote"
           containerStyle={localStyle.buttonContainer}
           buttonStyle={localStyle.button}
+          onPress={getQuote}
         />
       </View>
     </View>
@@ -32,5 +59,11 @@ const localStyle = StyleSheet.create({
     backgroundColor: "black",
     borderRadius: 6,
     width: "50%"
+  },
+  author: {
+    fontSize: 16,
+    alignSelf: "flex-end",
+    color: "black",
+    fontWeight: "700"
   }
 });
