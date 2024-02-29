2bbf1b537669452f613626fc735ceedbde84012d
antonio-digiteer <jerechoantoniosombrito.digiteer@gmail.com>
2024-02-29 17:43:23 +0800

    Minor code change: reduced code verbosing in Quotes Screen

--- src/screens/Facts.tsx
+++ src/screens/Facts.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { View, Text} from "react-native";
+import { View, Text } from "react-native";
 import { Button } from "@rneui/themed";
-import {styles} from '../styles/styles';
-
+import { styles } from "../styles/styles";
 
 type Fact = {
   text: string;
@@ -12,11 +11,9 @@ export const Facts = () => {
   const [fact, setFact] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
-
   const fetchFact = async () => {
     setLoading(true);
     try {
-
       const response = await fetch(
         "https://uselessfacts.jsph.pl/api/v2/facts/random"
       );
@@ -29,28 +26,28 @@ export const Facts = () => {
     }
   };
 
-
   useEffect(() => {
-
     setFact("Tap the button below to generate a fact.");
-  
   }, []);
 
   return (
-  
-  <View>
-    <View style={styles.apicontainer}>
-      <Text style={fact === "Tap the button below to generate a fact." ? styles.initialText : styles.apitext}>
-        {fact}
-      </Text>
-      <Button
-        title="Generate Fact"
-        containerStyle={styles.buttonContainer}
-        buttonStyle={styles.apibutton}
-        onPress={fetchFact}
-      />
+    <View>
+      <View style={styles.apicontainer}>
+        <Text
+          style={
+            fact === "Tap the button below to generate a fact."
+              ? styles.initialText
+              : styles.apitext
+          }>
+          {fact}
+        </Text>
+        <Button
+          title="Generate Fact"
+          containerStyle={styles.buttonContainer}
+          buttonStyle={styles.apibutton}
+          onPress={fetchFact}
+        />
+      </View>
     </View>
-  </View>
-
   );
 };
