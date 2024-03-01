4fda65fe9aa39801c013fc48f45bd582747fa741
karlrafael-digiteer <karlrafael.digiteer@gmail.com>
2024-03-01 12:28:19 +0800

    changes made

--- src/navigation/DrawerNavigator.tsx
+++ src/navigation/DrawerNavigator.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { Facts } from "../screens/Facts";
 import { Home } from "../screens/Home";
 import { Quotes } from "../screens/Quotes";
+import { Alert, BackHandler } from "react-native";
 
 const Drawer = createDrawerNavigator();
 
 export const DrawerNavigator = () => {
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      handleBackPress
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
+  const handleBackPress = () => {
+    Alert.alert("Exit App?", "Press 'Yes' to exit the app.", [
+      {
+        text: "Yes",
+        onPress: () => BackHandler.exitApp()
+      },
+      { text: "No", onPress: () => {} }
+    ]);
+    return true;
+  };
   return (
     <Drawer.Navigator
       initialRouteName="Home"
@@ -25,4 +45,5 @@ export const DrawerNavigator = () => {
       <Drawer.Screen name="Facts" component={Facts} />
     </Drawer.Navigator>
   );
+  
 };
